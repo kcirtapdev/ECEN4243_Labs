4d677b124b2cb4a4db5618e3ecf18b19728940ba
Ryan Stracener <ryanstrace@outlook.com>
2023-01-24 14:55:14 -0600

    mostly working

--- regfile.sv
+++ regfile.sv
@@ -2,38 +2,40 @@
  * Register Read/Write Project 
  * Authors: Ryan Stracener, Patrick Laverty
  *
+ * -----------------------------------------------------
+ *
  * Requirements:
  * (FINISHED)   1. Writes should take effect synchronously on the rising edge of the clock and only when write enable is also asserted (active high).
- *              2. The register file read port should output the value of the selected register combinatorially.
- *              3. The output of the register file read port should change after a rising clock edge if the selected register was modified by a write on the clock edge.
- *              4. Reading register zero ($0) should always return (combinatorially) the value zero.
- *              5. Writing register zero has no effect.
+ * (Finished)   2. The register file read port should output the value of the selected register combinatorially.
+ * (Finished)   3. The output of the register file read port should change after a rising clock edge if the selected register was modified by a write on the clock edge.
+ * (Finished)   4. Reading register zero ($0) should always return (combinatorially) the value zero.
+ * (Finished)   5. Writing register zero has no effect.
+ *
+ * -----------------------------------------------------
+ *
+ * Variable naming:
+ * `a` means address
+ * `d` means data
  */
 
 module regfile(
-    input logic clk,
-    input logic we3, // write enable
-    input logic [4:0] ra1, ra2, wa3, // two 5-bit src regs, and 1 5-bit dest reg
-    input logic [31:0] wd3, // write data
-    output logic [31:0] rd1, rd2 // read 1 and 2
+    input clk,
+    input we3, // write enable
+    input [4:0] ra1, ra2, wa3, // two 5-bit src regs, and one 5-bit dest reg
+    input [31:0] wd3, // write data
+    output [31:0] rd1, rd2 // read 1 and 2
     );
 
-    // a means address
-    // d means data
+    // Internal memory
+    reg [31:0] rf [31:0];
 
-    // logic [31:0] rf [31:0];
-    logic [31:0] rf;
+    // Set read ports to value, unless address is 0
+    assign rd1 = (ra1 == 5'd0) ? 32'd0 : rf[ra1];
+    assign rd2 = (ra2 == 5'd0) ? 32'd0 : rf[ra2];
 
     always @(posedge clk) begin
-        if(we3) begin
-            rf[wa3] <= wd3; // Setting internal register to write data
-            rd1 <= rf[ra1]; // Set output1 via address 1
-            rd2 <= rf[ra2]; // Set output2 via address 2
+        if(we3 && wa3 != 5'd0) begin
+            rf[wa3] <= wd3; // Set internal memory to "write data"
         end
     end
-
-    // three ported register file
-    // read two ports combinationally
-    // write third port on rising edge of clock
-    // register 0 hardwired to 0
 endmodule
